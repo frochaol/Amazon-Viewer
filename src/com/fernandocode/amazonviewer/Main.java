f3ac6c8ca7fc477e2207bb0e8a65098015aba53f
Fernando Rocha Olivera <frochaolivera18@gmail.com>
2019-05-12 22:05:31 -0500

    FIRST COMMIt

---
+++ src/com/fernandocode/amazonviewer/Main.java
@@ -0,0 +1,25 @@
+package com.fernandocode.amazonviewer;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		int exit = 0;
+		
+		do {		
+			System.out.println("Bienvenidos a Amazon Viewer");
+			System.out.println("");
+			System.out.println("Selecciona el número de la opción deseada");
+			System.out.println("1. Movies");
+			System.out.println("2. Seriess");
+			System.out.println("3. Libros");
+			System.out.println("4. Revistas");
+			System.out.println("0. Exit");
+			
+		} while (exit != 0);
+
+	}
+
+}
